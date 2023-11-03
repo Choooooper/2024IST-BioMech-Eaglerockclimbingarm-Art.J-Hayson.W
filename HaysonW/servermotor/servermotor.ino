ddbf0c03042d9575ecd1895ce4dbd91f242d16f1
Choooooper <hayson.wong@education.nsw.gov.au>
2023-11-03 11:49:39 +1100

    update server motor ino & ultrasonic ino

--- HaysonW/servermotor/servermotor.ino
+++ HaysonW/servermotor/servermotor.ino
@@ -1,11 +1,22 @@
+/*
+  Purpose: Basic example of a 180deg Micro Servo connected via a Seead Screw Terminal
+  Notes: 
+    1. Connect to a Digital Pin
+    2. 180 Degree ROM 0-180 values
+  Author: Ben Jones 13/7/23
+  Contact: benjmain.jones21@det.nsw.edu.au
+  Source: https://wiki.seeedstudio.com/Grove-Servo/
+*/
+
 #include <Servo.h>
 
 Servo myServo;  // create servo object to control a servo
 
+
 static unsigned int degreeOfMovement = 180;    // variable to read the value from the analog pin
 
 void setup() {
-  myServo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myServo.attach(8);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
